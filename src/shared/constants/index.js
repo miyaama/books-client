c891e837176268a648497b20c86e0c95b71fe16a
Anna Obikhodova <a.obikhodova@andersenlab.com>
2022-10-27 15:52:41 +0400

    fix url

--- src/shared/constants/index.js
+++ src/shared/constants/index.js
@@ -4,7 +4,7 @@ export * from "./locales";
 export const ACCESS_USER = "user";
 export const ACCESS_ADMIN = "admin";
 
-export const BACKEND_URL = "https://colections-book-server.herokuapp.com/";
+export const BACKEND_URL = "https://colections-book-server.herokuapp.com";
 
 export const LIGHT_THEME = "LIGHT_THEME";
 export const DARK_THEME = "DARK_THEME";
