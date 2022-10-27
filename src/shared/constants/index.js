a309cfe570d288cf387576dd949a6aa4da9b9fb4
Anna Obikhodova <a.obikhodova@andersenlab.com>
2022-10-27 15:51:12 +0400

    change backend url to production

--- src/shared/constants/index.js
+++ src/shared/constants/index.js
@@ -4,7 +4,7 @@ export * from "./locales";
 export const ACCESS_USER = "user";
 export const ACCESS_ADMIN = "admin";
 
-export const BACKEND_URL = "http://localhost:5000";
+export const BACKEND_URL = "https://colections-book-server.herokuapp.com/";
 
 export const LIGHT_THEME = "LIGHT_THEME";
 export const DARK_THEME = "DARK_THEME";
