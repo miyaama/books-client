b2e94d2452aa4f13a591a1e80eeebcd17c1685a5
Anna Obikhodova <a.obikhodova@andersenlab.com>
2023-02-13 14:05:08 +0400

    add spin

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,14 +1,34 @@
+import { useSelector } from "react-redux";
+import { Spin } from "antd";
+
 import PageLayout from "../../components/PageLayout";
 import TagsCloud from "./TagsCloud/";
 import HomeTable from "./HomeTable/HomeTable";
 import LargestCollections from "./LargestCollections";
 
 const HomePage = () => {
+  const { items, collections } = useSelector((state) => state.home);
+  const isLoading =
+    items.length != 0 && collections.length != 0;
+
   return (
     <PageLayout>
-      <LargestCollections />
-      <HomeTable />
-      <TagsCloud />
+      {isLoading ? (
+        <>
+          <LargestCollections />
+          <HomeTable />
+          <TagsCloud />
+        </>
+      ) : (
+        <div
+          style={{
+            margin: "100px 0",
+            textAlign: "center",
+          }}
+        >
+          <Spin />
+        </div>
+      )}
     </PageLayout>
   );
 };
